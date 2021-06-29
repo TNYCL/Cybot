a6f3085be591ab51b10bb332d0d3704c08176fa2
TNYCL <73045039+TNYCL@users.noreply.github.com>
2021-06-29 17:16:11 +0300

    Update Options.java

--- src/cybot/Options.java
+++ src/cybot/Options.java
@@ -5,26 +5,25 @@ import java.util.List;
 
 public class Options {
 	
-	public static String websiteLink = "https://www.tnycl.com/";
+	public static String pullLink = "https://www.tnycl.com/";
 	
-	public static String token = "ODUzNTU3NzkyOTg4NTk0MTg2.YMXHpA._5QrrZBS3DIuXQEEdSzCylMEM9I";
+	public static String token = "TOKEN";
 	public static String prefix = "c.";
 	
 	public static String tweetChannelID = "853559135642583043";
 	public static String broadcastChannelID = "853767722827251753";
 	public static String commandChannelID = "853995805592256553";
 	
-	public static String sqlIP = "45.87.81.145";
-	public static String sqlPort = "3306";
-	public static String sqlPassword = "i#AU:~V$a@8";
-	public static String sqlDatabase = "u870149946_tnyclcom";
-	public static String sqlUser = "u870149946_tnycl";
+	public static String sqlIP = null;
+	public static String sqlPort = null;
+	public static String sqlPassword = null;
+	public static String sqlDatabase = null;
+	public static String sqlUser = null;
 	
 	public static int activityChangeSeconds = 60;
 	
 	public static List<String> activityList = Arrays.asList(new String[] {
-			"Minecraft",
-			"Cypress Games"
+			"Minecraft"
 	});
 
 }
